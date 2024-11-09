f3fdab8c5a427b1393ea6171ea5e3c308d99edc2
Zachary Djerdjouri <zdjer050@uottawa.ca>
2024-11-08 21:06:46 -0500

    base

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(props) {
+
+  return (
+    <>
+      <header>
+        <h1>{props.greeting}{props.subject} !</h1>
+        <button type='button' className='primary'>Click Me</button>
+      </header>
+    </>
+  );
+}
+
+export default App
